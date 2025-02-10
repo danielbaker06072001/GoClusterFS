5b25c049daafe6fefea366494a5235db79c1595f
Duc Ng <duc.nguyen@weallnet.com>
2025-02-10 00:29:57 -0500

    fix test delee file was open

---
+++ storage_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+)
+
+func TestPathTransformFunc(t *testing.T) {
+	key := "momsbestpicture"
+	pathKey := CASPathTransformFunc(key)
+	expectedOriginalKey := "6804429f74181a63c50c3d81d733a12f14a353ff"
+	expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+	if pathKey.Pathname != expectedPathName {
+		t.Errorf("have %s wants %s", pathKey.Pathname, expectedPathName)
+	}
+	if pathKey.Filename != expectedOriginalKey {
+		t.Errorf("have %s wants %s", pathKey.Filename, expectedOriginalKey)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc,
+	}
+	s := NewStore(opts)
+	key := "momspecials"
+	data := []byte("some jpg bytes")
+
+	if err := s.writeStrean(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
+	}
+
+}
+
+func TestStore(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTransformFunc,
+	}
+	s := NewStore(opts)
+	key := "momspecials"
+	data := []byte("some jpg bytes")
+
+	if err := s.writeStrean(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	r, err := s.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, err := ioutil.ReadAll(r)
+	if string(b) != string(data) {
+		t.Errorf("want %s have %s", data, b)
+	}
+
+}
